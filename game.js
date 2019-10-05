7c3b9861bbf8c13dd1d3f7c83c112d45388acb3d
Bailey Nguyen <baileythuong@gmail.com>
2019-10-05 12:14:22 +0700

    implement username in local storage

--- game.js
+++ game.js
@@ -1,17 +1,27 @@
-/*
-  Code modified from:
-  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
-  using graphics purchased from vectorstock.com
-*/
-
-/* Initialization.
-Here, we create and add our "canvas" to the page.
-We also load all of our images. 
-*/
+// reset the game
 function reset() {
   location.reload();
 }
 
+// let user submit username
+function submitName() {
+  let userInputName = document.getElementById("nameInput").value;
+
+  let player = document.getElementById("playerName");
+  player.innerHTML = `G'day, ${userInputName || "Obi Wan Kenobi"}!`
+  closeForm("myForm");
+}
+
+// close form after submission
+function closeForm(element) {
+  document.getElementById(element).style.display = "none";
+}
+
+// submit button
+let submitButton = document.getElementById("submitBtn");
+submitButton.addEventListener("click", submitName);
+
+// initialization
 let canvas;
 let ctx;
 
@@ -21,14 +31,16 @@ ctx.font = "30px Arial";
 
 canvas.width = 512;
 canvas.height = 480;
-document.body.appendChild(canvas);
+document.getElementById('canvas').appendChild(canvas);
 
-let bgReady, aimReady, craftReady, craft1Ready;
-let bgImage, aimImage, craftImage, craft1Image;
+let bgReady, aimReady, craftReady;
+let bgImage, aimImage, craftImage;
 
 let startTime = Date.now();
 const SECONDS_PER_ROUND = 30;
 let elapsedTime = 0;
+
+// crafts
 let lst = [
   "images/Ship01.png",
   "images/Ship02.png",
@@ -37,18 +49,22 @@ let lst = [
   "images/Ship05.png",
   "images/Ship06.png"
 ];
+
+// randomnize crafts
 let item = lst[Math.floor(Math.random() * lst.length)];
 
+
+// load images
 function loadImages() {
   bgImage = new Image();
   bgImage.onload = function() {
-    // show the background image
+    // show background image
     bgReady = true;
   };
   bgImage.src = "images/backgroundsky.png";
   aimImage = new Image();
   aimImage.onload = function() {
-    // show the hero image
+    // show the gun aim image
     aimReady = true;
   };
   aimImage.src = "images/aim_V3.png";
@@ -76,16 +92,7 @@ function loadImages() {
   gameOverImage.src = "images/message_gameover.png";
 }
 
-/**
- * Setting up our characters.
- *
- * Note that aimX represents the X position of our hero.
- * aimY represents the Y position.
- * We'll need these values to know where to "draw" the hero.
- *
- * The same applies to the monster.
- */
-
+// represent the X and Y positions of gun aim
 let aimX = canvas.width / 2;
 let aimY = canvas.height / 2;
 
@@ -95,12 +102,13 @@ let craftY = 100;
 let score = 0;
 let isGameOver = false;
 
+// store user's score, name and hostory on local storage
 function getAppState() {
   return (
     JSON.parse(localStorage.getItem("appState")) || {
       gameHistory: [],
       currentHighScore: 0,
-      currentUser: document.getElementById("username") || "Anonymous"
+      currentUser: document.getElementById("currentUser")
     }
   );
 }
@@ -109,20 +117,16 @@ function save(appState) {
   return localStorage.setItem("appState", JSON.stringify(appState));
 }
 
+// show username and highest scores on browser
 document.getElementById("highScore").innerHTML = `Highest Score: ${
-  getAppState().currentHighScore
-}`;
+  getAppState().currentHighScore}`;
 
-/**
- * Keyboard Listeners
- * You can safely ignore this part, for now.
- *
- * This is just to let JavaScript know when the user has pressed a key.
- */
+document.getElementById("currentUser").innerHTML = `Captain: ${getAppState().userInputName || "Obi Wan Kenobi"}`
+
+// keyboard listeners
 let keysDown = {};
 function setupKeyboardListeners() {
-  // Check for keys pressed where key represents the keycode captured
-  // For now, do not worry too much about what's happening here.
+  // check for keys pressed where key represents the keycode captured
   addEventListener(
     "keydown",
     function(key) {
@@ -145,14 +149,7 @@ function updateMonterPos() {
   craftY = Math.floor(Math.random() * 400 - 10 + 1) + 10;
 }
 
-/**
- *  Update game objects - change player position based on key pressed
- *  and check to see if the monster has been caught!
- *
- *  If you change the value of 5, the player will move at a different rate.
- */
-
-//
+// update game objects - change player position based on key pressed
 let update = function() {
   elapsedTime = Math.floor((Date.now() - startTime) / 1000);
   isGameOver = elapsedTime > SECONDS_PER_ROUND;
@@ -162,44 +159,43 @@ let update = function() {
   // }
 
   if (38 in keysDown) {
-    // Player is holding up key
+    // player is holding up key
     aimY -= 5;
   }
   if (40 in keysDown) {
-    // Player is holding down key
+    // player is holding down key
     aimY += 5;
   }
   if (37 in keysDown) {
-    // Player is holding left key
+    // player is holding left key
     aimX -= 5;
   }
   if (39 in keysDown) {
-    // Player is holding right key
+    // player is holding right key
     aimX += 5;
   }
 
-  // Hero going left off screen
+  // prevent gun aim from going left off screen
   if (aimX <= 0) {
     aimX = canvas.width - 10;
   }
 
-  // Hero going right off screen
+  // prevent gun aim from going right off screen
   if (aimX >= canvas.width) {
     aimX = 0;
   }
 
-  // Hero going up off screen
+  // prevent gun aim from going up off screen
   if (aimY <= 0) {
     aimY = canvas.height - 10;
   }
 
-  // Hero going down off screen
+  // prevent gun aim from going down off screen
   if (aimY >= canvas.height) {
     aimY = 0;
   }
 
-  // Check if player and monster collided. Our images
-  // are about 32 pixels big.
+  // check if gun aim and craft collided
   // console.log('keysDown', keysDown)
   const aimedAtCraft =
     aimX <= craftX + 32 &&
@@ -207,8 +203,7 @@ let update = function() {
     aimY <= craftY + 32 &&
     craftY <= aimY + 32;
   if (aimedAtCraft) {
-    score += 1;
-    if (shootFX()) {
+      score += 1;
       craftX = Math.floor(Math.random() * canvas.width - 10);
       craftY = Math.floor(Math.random() * canvas.height - 10);
       craftImage.src = lst[Math.floor(Math.random() * lst.length)];
@@ -222,28 +217,20 @@ let update = function() {
     if (appState.currentHighScore < score) {
       appState.currentHighScore = score;
       save(appState);
-      document.getElementById("highScore").innerHTML = score;
+      document.getElementById("highScore").innerHTML = `Highest Score: ${score}`;
     }
-  }
-};
 
-function shootFX() {
-  setTimeout(hitCraft, 500);
+    let userInputName = document.getElementById("nameInput").value;
+    let currentUser = appState.currentUser = userInputName;
+    save(appState);
+    document.getElementById("currentUser").innerHTML = `Captain: ${currentUser || "Obi Wan Kenobi"}`;
+  };
 
-  return true;
-}
-
-function hitCraft() {
-  craftImage = fxImage;
-}
-
-/**
- * This function, render, runs as often as possible.
- */
+// this function, render, runs as often as possible.
 let render = function() {
   ctx.fillStyle = "#ffffff";
   ctx.font = "20px 'Turret Road'";
-
+  
   if (!isGameOver) {
     //   if (score >= 20) {
     //     ctx.fillText(`YOU WIN`, 200, 250);
@@ -258,41 +245,28 @@ let render = function() {
     if (craftReady) {
       ctx.drawImage(craftImage, craftX, craftY);
     }
-    // ctx.fillText(
-    //   `Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`,
-    //   20,
-    //   100
-    // );
-
+    
     document.getElementById("seconds").innerHTML = `Timer: ${SECONDS_PER_ROUND -
-      elapsedTime}`;
-
+      elapsedTime}`
     // }
     // set game over
   } else {
-    ctx.drawImage(gameOverImage, 300, 300);
+    ctx.drawImage(gameOverImage, 200, 200);
     isGameOver = true;
   }
 };
-
-/**
- * The main game loop. Most every game will have two distinct parts:
- * update (updates the state of the game, in this case our hero and monster)
- * render (based on the state of our game, draw the right things)
- */
+ // the main game loop. Most every game will have two distinct parts: update (updates the state of the game, in this case our gun aim and craft) and render (based on the state of our game, draw the right things
 let main = function() {
   update();
   render();
-
-  // Request to do this again ASAP. This is a special method
-  // for web browsers.
+  // request to do this again ASAP. this is a special method for web browsers
   if (!isGameOver) {
     requestAnimationFrame(main);
   }
 };
 
-// Cross-browser support for requestAnimationFrame.
-// Safely ignore this line. It's mostly here for people with old web browsers.
+// cross-browser support for requestAnimationFrame
+// safely ignore this line. It's mostly here for people with old web browsers
 let w = window;
 requestAnimationFrame =
   w.requestAnimationFrame ||
@@ -300,8 +274,7 @@ requestAnimationFrame =
   w.msRequestAnimationFrame ||
   w.mozRequestAnimationFrame;
 
-// Let's play this game!
+// let's play this game!
 // setInterval(updateMonterPos, 2000);
 loadImages();
 setupKeyboardListeners();
-main();
